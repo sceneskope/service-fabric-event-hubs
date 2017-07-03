2ddfd861387f19f0144e1d711f7d5eca8b5c402c
Nick Randell <nick@nickrandell.com>
2017-07-03 07:19:41 +0100

    Added a simple reader to handle offset saving later

---
+++ src/SceneSkope.ServiceFabric.EventHubs/SimpleReadingReceiver.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure.EventHubs;
+using Microsoft.ServiceFabric.Data;
+using Microsoft.ServiceFabric.Data.Collections;
+using Serilog;
+
+namespace SceneSkope.ServiceFabric.EventHubs
+{
+    public abstract class SimpleReadingReceiver : IPartitionReceiveHandler
+    {
+        public ILogger Log { get; }
+
+        private readonly IReliableDictionary<string, string> _offsets;
+        protected readonly string _partition;
+
+        public virtual int MaxBatchSize => 100;
+
+        protected SimpleReadingReceiver(ILogger log, IReliableDictionary<string, string> offsets, string partition)
+        {
+            Log = log.ForContext("partition", partition);
+            _offsets = offsets;
+            _partition = partition;
+        }
+
+        public virtual Task InitialiseAsync() => Task.FromResult(true);
+
+        public virtual Task ProcessErrorAsync(Exception error)
+        {
+            Log.Error(error, "Error reading: {exception}", error.Message);
+            return Task.FromResult(true);
+        }
+
+        protected abstract Task ProcessEventAsync(EventData @event);
+
+        public async Task ProcessEventsAsync(IEnumerable<EventData> events)
+        {
+            if (events == null)
+            {
+                return;
+            }
+
+            var count = Log.IsEnabled(Serilog.Events.LogEventLevel.Verbose) ? events.Count() : 0;
+            Log.Verbose("Got {count} events to process", count);
+            foreach (var @event in events)
+            {
+                await ProcessEventAsync(@event).ConfigureAwait(false);
+            }
+            Log.Verbose("Processed {count} events", count);
+        }
+
+        protected Task SaveOffsetAsync(ITransaction tx, string latestOffset) => _offsets.SetAsync(tx, _partition, latestOffset);
+    }
+}
