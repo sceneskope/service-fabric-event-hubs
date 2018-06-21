352f57871152cdf610d00a6f07dbbb11a81001a3
Nick Randell <nick.randell@sceneskope.com>
2018-06-21 11:09:34 +0100

    Added a few more configuration options

--- src/SceneSkope.ServiceFabric.EventHubs/EventHubConfiguration.cs
+++ src/SceneSkope.ServiceFabric.EventHubs/EventHubConfiguration.cs
@@ -10,18 +10,28 @@ namespace SceneSkope.ServiceFabric.EventHubs
     {
         public static async Task<EventHubClient> GetEventHubClientAsync(string sectionName, Action<string> onFailure, CancellationToken ct)
         {
+            var inputConnectionString = await GetEventHubConnectionString(sectionName, onFailure, ct).ConfigureAwait(false);
+            return EventHubClient.CreateFromConnectionString(inputConnectionString);
+        }
+
+        private static async Task<string> GetEventHubConnectionString(string sectionName, Action<string> onFailure, CancellationToken ct)
+        {
+            return (await GetEventHubConnectionStringBuilder(sectionName, onFailure, ct).ConfigureAwait(false)).ToString();
+        }
+
+        private static async Task<EventHubsConnectionStringBuilder> GetEventHubConnectionStringBuilder(string sectionName, Action<string> onFailure, CancellationToken ct)
+        {
             var configuration = new FabricConfigurationProvider(sectionName);
             if (!configuration.HasConfiguration)
             {
                 await configuration.RejectConfigurationAsync($"No {sectionName} section", onFailure, ct).ConfigureAwait(false);
             }
-            var inputConnectionString = new EventHubsConnectionStringBuilder(
+            return new EventHubsConnectionStringBuilder(
                 new Uri(await configuration.TryReadConfigurationAsync("EndpointAddress", onFailure, ct).ConfigureAwait(false)),
                 await configuration.TryReadConfigurationAsync("EntityPath", onFailure, ct).ConfigureAwait(false),
                 await configuration.TryReadConfigurationAsync("SharedAccessKeyName", onFailure, ct).ConfigureAwait(false),
                 await configuration.TryReadConfigurationAsync("SharedAccessKey", onFailure, ct).ConfigureAwait(false)
             );
-            return EventHubClient.CreateFromConnectionString(inputConnectionString.ToString());
         }
     }
 }
